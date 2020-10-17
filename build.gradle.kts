f6795523dd65ab49bab3a87c6c4fd4c06fcc8776
Jan Müller <janmueller3698@gmail.com>
2020-10-17 14:19:42 +0200

    Created project

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    kotlin("js") version "1.4.10"
+}
+group = "eu.yeger"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlinx")
+    }
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlin-js-wrappers")
+    }
+    maven {
+        url = uri("https://dl.bintray.com/deryeger/maven")
+    }
+}
+dependencies {
+    testImplementation(kotlin("test-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-1.4.10")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.4.10")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.110-kotlin-1.4.10")
+    implementation("eu.yeger:cyk-algorithm:0.1.0")
+}
+kotlin {
+    js {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+    }
+}
